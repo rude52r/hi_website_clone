99cb5ddd969c11f387f63c4270e955111fefac22
rude52r <sooil5001@gmail.com>
2024-01-19 17:49:18 +0900

    modified:   sulwhasoo/css/layout.css 	modified:   sulwhasoo/index.html 	modified:   sulwhasoo/js/main.js

--- sulwhasoo/js/main.js
+++ sulwhasoo/js/main.js
@@ -4,4 +4,13 @@ $(function(){
             $(this).toggleClass("show")
         })
 
+        $(".sub").hide();
+        $(".gnb_menu > li").hover(function(){
+            $(this).find(".sub").stop().slideDown();
+            $(".gnb_menu").stop().animate({height:"80px"}, 500)
+        }, function(){
+            $(this).find(".sub").stop().slideUp();
+            $(".gnb_menu").stop().animate({height:"50px"}, 500)
+        })
+
 })//jquery
